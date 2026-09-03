a68ad87154cac5e1e769f3bb203e8da9e0b9339f
Kristanto Sean <kristantosean@gmail.com>
2026-09-03 12:28:40 +0700

    Initial Commit

---
+++ PerkCoffee/PerkCoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PerkCoffeeApp.swift
+//  PerkCoffee
+//
+//  Created by Kristanto Sean on 2026-09-03.
+//
+
+import SwiftUI
+
+@main
+struct PerkCoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
